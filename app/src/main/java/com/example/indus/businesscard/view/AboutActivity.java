6aced171e9e0f3f1a9ff047373b50ce12f4ae801
industradamus <industradamus@gmail.com>
2018-11-25 00:48:34 +0300

    Переделал визитную карточку

--- app/src/main/java/com/example/indus/businesscard/view/AboutActivity.java
+++ app/src/main/java/com/example/indus/businesscard/view/AboutActivity.java
@@ -1,6 +1,5 @@
 package com.example.indus.businesscard.view;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,10 +7,9 @@ import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.indus.businesscard.R;
@@ -19,20 +17,15 @@ import com.example.indus.businesscard.R;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String MY_MAIL = "buvaka.sergey89@gmail.com";
-    private static final String MY_WEB = "https://hh.ru/applicant/resumes/view?resume=38b9e174ff05bed6f30039ed1f634e4b473539";
-    private static final String MY_NUMBER = "89689613600";
-    private static final String MY_INSTAGRAM = "https://www.instagram.com/neformalniy_papasha/";
-    private static final String MY_FACEBOOK = "https://www.facebook.com/profile.php?id=100010244763199";
+    private static final String MY_WEB = "https://github.com/industradamus";
+    private static final String MY_NUMBER = "+79689613600";
 
-    private TextView myPhoneNumber;
-    private EditText messageEnter;
-    private Button sendMessageBtn;
-    private ImageView myResumeWebLink;
-    private ImageView instagramLogo, whatsAppLogo, facebookLogo;
+    private RelativeLayout phoneLayout, mailLayout, webLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +33,7 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_about);
 
         initView();
-        viewSetFocusable();
         setClickListeners();
-        addDisclaimer();
         createToolbar();
     }
 
@@ -59,35 +50,26 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.facebook_logo:
-                openFacebook();
-                break;
-            case R.id.instagram_logo:
-                openInstagram();
-                break;
-            case R.id.my_phone_number:
+
+            case R.id.phone_layout:
                 callMe();
                 break;
-            case R.id.my_web_link:
-                openResume();
-                break;
-            case R.id.send_message_btn:
+
+            case R.id.mail_layout:
                 sendEmail();
                 break;
-            case R.id.whatsapp_logo:
-                openWhatsApp();
+
+            case R.id.web_layout:
+                openResume();
                 break;
         }
     }
 
     private void initView() {
-        myPhoneNumber = findViewById(R.id.my_phone_number);
-        myResumeWebLink = findViewById(R.id.my_web_link);
-        messageEnter = findViewById(R.id.message_enter);
-        sendMessageBtn = findViewById(R.id.send_message_btn);
-        instagramLogo = findViewById(R.id.instagram_logo);
-        whatsAppLogo = findViewById(R.id.whatsapp_logo);
-        facebookLogo = findViewById(R.id.facebook_logo);
+        phoneLayout = findViewById(R.id.phone_layout);
+        mailLayout = findViewById(R.id.mail_layout);
+        webLayout = findViewById(R.id.web_layout);
+
     }
 
     private void createToolbar() {
@@ -100,31 +82,11 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private void viewSetFocusable() {
-        myPhoneNumber.setFocusable(true);
-        myResumeWebLink.setFocusable(true);
-        instagramLogo.setFocusable(true);
-        whatsAppLogo.setFocusable(true);
-        facebookLogo.setFocusable(true);
-    }
-
     private void setClickListeners() {
-        myPhoneNumber.setOnClickListener(this);
-        myResumeWebLink.setOnClickListener(this);
-        sendMessageBtn.setOnClickListener(this);
-        instagramLogo.setOnClickListener(this);
-        whatsAppLogo.setOnClickListener(this);
-        facebookLogo.setOnClickListener(this);
-    }
+        phoneLayout.setOnClickListener(this);
+        mailLayout.setOnClickListener(this);
+        webLayout.setOnClickListener(this);
 
-    private void openFacebook() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MY_FACEBOOK));
-        startActivity(intent);
-    }
-
-    private void openInstagram() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MY_INSTAGRAM));
-        startActivity(intent);
     }
 
     private void callMe() {
@@ -139,34 +101,11 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void sendEmail() {
-        String message = messageEnter.getText().toString();
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:"));
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]{MY_MAIL});
-        intent.putExtra(Intent.EXTRA_TEXT, message);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(Intent.createChooser(intent, "Send Email"));
         }
-    }
-
-    @SuppressLint("IntentReset")
-    private void openWhatsApp() {
-        String uriSMS = "smsto:" + MY_NUMBER;
-        Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(uriSMS));
-        intent.setType("text/plain");
-        intent.setPackage("com.whatsapp");
-        startActivity(Intent.createChooser(intent, "Share with"));
-    }
-
-    private void addDisclaimer() {
-        LinearLayout linearLayout = findViewById(R.id.scroll_layout);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
-                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(8,8,8,8);
-        TextView textView = new TextView(this);
-        textView.setText(R.string.disclaimer);
-        textView.setGravity(Gravity.END);
-        textView.setLayoutParams(layoutParams);
-        linearLayout.addView(textView);
     }
 }
